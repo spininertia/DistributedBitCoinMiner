ac47a4b73cfef271fef04961705a522f7dfa530b
Siping <hotcoderj@gmail.com>
2014-03-01 13:52:03 -0500

    finish miner

--- src/github.com/cmu440/bitcoin/miner/miner.go
+++ src/github.com/cmu440/bitcoin/miner/miner.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"github.com/cmu440/bitcoin"
+	"github.com/cmu440/lsp"
 	"os"
 )
 
@@ -12,5 +15,44 @@ func main() {
 		return
 	}
 
-	// TODO: implement this!
+	hostport := os.Args[1]
+
+	miner := lsp.NewClient(hostport, lsp.NewParams())
+	defer miner.Close()
+
+	joinReq := json.Marshal(bitcoin.NewJoin())
+	miner.Write(joinReq)
+
+	msg := new(bitcoin.Message)
+	for {
+		payload, err := miner.Read()
+		if err != nil {
+			break
+		}
+		json.Unmarshal(payload, msg)
+
+		// mining
+		minHash, nonce := mine(msg.Data, msg.Lower, msg.Upper)
+		err = miner.Write(bitcoin.NewResult(minHash, nonce))
+
+		if err != nil {
+			break
+		}
+	}
+
+}
+
+func mine(data string, lower, upper int) (uint64, uint64) {
+	minHash := ^uint64(0)
+	var nonce uint64
+
+	for i := lower; i <= upper; i++ {
+		hash := bitcoin.Hash(data, i)
+		if hash < minHash {
+			minHash = hash
+			nonce = i
+		}
+	}
+
+	return minHash, nonce
 }
